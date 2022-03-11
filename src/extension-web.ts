00e7863fbe3e3651e2468405f1e6099a1763c8a4
Alessandro Fragnani <alefragnani@hotmail.com>
2022-03-10 23:53:55 -0300

    Add web support (WIP - extensionDependencies issue)

---
+++ src/extension-web.ts
@@ -0,0 +1,18 @@
+/*---------------------------------------------------------------------------------------------
+*  Copyright (c) Alessandro Fragnani. All rights reserved.
+*  Licensed under the MIT License. See License.md in the project root for license information.
+*--------------------------------------------------------------------------------------------*/
+
+import * as vscode from 'vscode';
+
+import { Container } from './container';
+import { registerWalkthrough } from "./commands/walkthrough";
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+    Container.context = context;
+    
+    registerWalkthrough();
+}
