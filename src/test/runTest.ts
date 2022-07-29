c83d19d7f67bc32ca52289cf6665b64e410eb321
Alessandro Fragnani <alefragnani@hotmail.com>
2022-07-28 22:17:19 -0300

    :lipstick: nothing

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -3,7 +3,7 @@ import * as path from 'path';
 import { runTests } from '@vscode/test-electron';
 
 async function main() {
-	try {
+	try { 
 		// The folder containing the Extension Manifest package.json
 		// Passed to `--extensionDevelopmentPath`
 		const extensionDevelopmentPath = path.resolve(__dirname, '../../../');
