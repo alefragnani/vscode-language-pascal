5ca24f7a75191587715f4b6b9add2ce82e9baea9
Alessandro Fragnani <alefragnani@hotmail.com>
2025-02-16 13:55:43 -0300

    Add New File support (WIP)

--- src/extension.ts
+++ src/extension.ts
@@ -10,11 +10,11 @@ import { registerWhatsNew } from './whats-new/commands';
 import { registerGenerateTags } from './commands/generateTags';
 import { registerProviders } from './providers';
 import { registerWalkthrough } from "./commands/walkthrough";
+import { commands } from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export async function activate(context: vscode.ExtensionContext) {
-
     Container.context = context;
     
     await registerWhatsNew();
@@ -22,9 +22,30 @@ export async function activate(context: vscode.ExtensionContext) {
     registerGenerateTags();
     registerWalkthrough();
 
+    commands.registerCommand("pascal.newFile", async () => newFile());
+
     vscode.workspace.onDidGrantWorkspaceTrust(() => {
         registerProviders();
         registerGenerateTags();
     })
     
+}
+
+async function regularNewFile() {
+    const doc = await vscode.workspace.openTextDocument(
+        { language: "pascal", content: "program HelloWorld;\nbegin\n  WriteLn('Hello, World!');\nend." });
+    await vscode.window.showTextDocument(doc);
+}
+async function newFile() {
+    const doc = await vscode.workspace.openTextDocument({ language: "pascal" });
+    const editor = await vscode.window.showTextDocument(doc);
+
+    const snippet = new vscode.SnippetString(
+        "program ${1:ProgramName};\n" +
+        "begin\n" +
+        "  WriteLn('${2:Hello, World!}');\n" +
+        "end."
+    );
+
+    editor.insertSnippet(snippet);
 }
